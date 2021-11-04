8532c6d2813833b678f811c8eae5eb01bd25b77c
Owais <owaisbinasad.work@gmail.com>
2021-11-04 21:34:45 +0500

    Basic flight working. Dynamic generation next.

--- practice.js
+++ practice.js
@@ -3,9 +3,17 @@ let canvas = document.getElementById("webgl-canvas");
 let gl = canvas.getContext("webgl2");
 let program;
 
-let ENABLE_PERLIN = false;
-let ENABLE_ROUNDING = true;
-let TERRAIN_DETAIL_LEVEL = 10;
+let ENABLE_PERLIN = true;
+let ENABLE_ROUNDING = false;
+let TERRAIN_DETAIL_LEVEL = 200;
+let MESH_ELEVATION = 0;
+let TERRAIN_BOUNDS = 20;
+let NEAR = 0.1;
+let FAR = 1000.0;
+let PERLIN_SCALING_FACTOR = 2.0;
+let FLYING_SPEED = 1.0;
+let YAW_SENSITIVITY = 1;
+let YAW_CONTROL = false;
 
 // Init WebGL
 gl.viewport(0, 0, canvas.width, canvas.height);
@@ -16,58 +24,77 @@ gl.enable(gl.DEPTH_TEST);
 // gl.frontFace(gl.CCW);
 // gl.cullFace(gl.BACK);
 
+//Place this clear color settings
+gl.polygonOffset(1,1);
+
+//enable or disable this setting as needed
+gl.enable(gl.POLYGON_OFFSET_FILL);
+
 program = initShaders(gl, "vertex-shader", "fragment-shader");
 gl.useProgram(program);
 
 gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
 
-var points = []; var colors = []; var matrix = []; var temp = [];
+window.onload = () => {
+    document.onkeydown = (e) => {
+        if (e.key === "a" || e.key === "A") {
+            angle = angle + (YAW_CONTROL ? YAW_SENSITIVITY : 0);
+        }
+        else if (e.key === "d" || e.key === "D") {
+            angle = angle - (YAW_CONTROL ? YAW_SENSITIVITY : 0);
+        }
+    }
+}
+
+var points = [];
+var colors = [];
+var matrix = [];
+var temp = [];
+
+let i = 0;
 
 
 function get_patch(x_min, x_max, z_min, z_max){
-    var x = x_min; var z = z_min;
-    var x_interval = (x_max - x_min)/TERRAIN_DETAIL_LEVEL; var z_interval = (z_max - z_min)/TERRAIN_DETAIL_LEVEL;
-
-    console.log(x_interval);
+    
+    var x_interval = (x_max - x_min)/TERRAIN_DETAIL_LEVEL;
+    var z_interval = (z_max - z_min)/TERRAIN_DETAIL_LEVEL;
     
     var temp_points = [];
 
-    for (let j= 0; j < TERRAIN_DETAIL_LEVEL; j ++){
-        for (let i = 0; i < TERRAIN_DETAIL_LEVEL; i++){
+    for (let j= z_min; j < z_max; j = j + z_interval){
+        for (let i = x_min; i < x_max; i = i + x_interval){
             
-            a_y = ENABLE_PERLIN ? perlin.get(x, z) : 0; 
-            A = vec3(x, a_y, z); a = vec3(0,0,0);
+            a_y = ENABLE_PERLIN ? (PERLIN_SCALING_FACTOR * perlin.get(i, j)) : 0; 
+            A = vec3(i, a_y, j);
+            A_m = vec3(i, a_y + MESH_ELEVATION, j);
             
-            b_y = ENABLE_PERLIN ? perlin.get(x, round(z + z_interval)) : 0; 
-            B = vec3(x, b_y, round(z + z_interval)); b = vec3(0,0,0);
+            b_y = ENABLE_PERLIN ? (PERLIN_SCALING_FACTOR * perlin.get(i,j + z_interval)) : 0; 
+            B = vec3(i, b_y, j + z_interval);
+            B_m = vec3(i, b_y + MESH_ELEVATION, j + z_interval);
             
-            c_y = ENABLE_PERLIN ? perlin.get(round(x + x_interval), round(z + z_interval)) : 0;
-            C = vec3(round(x + x_interval), c_y, round(z + z_interval)); c = vec3(0,0,0);
+            c_y = ENABLE_PERLIN ? (PERLIN_SCALING_FACTOR * perlin.get(i + x_interval, j + z_interval)) : 0;
+            C = vec3(i + x_interval, c_y, j + z_interval);
+            C_m = vec3(i + x_interval, c_y + MESH_ELEVATION, j + z_interval);
             
-            d_y = ENABLE_PERLIN ? perlin.get(round(x + x_interval), z) : 0;  
-            D = vec3(round(x + x_interval), d_y  , z); d = vec3(0,0,0);
+            d_y = ENABLE_PERLIN ? (PERLIN_SCALING_FACTOR * perlin.get(i + x_interval, j)) : 0;  
+            D = vec3(i + x_interval, d_y, j);
+            D_m = vec3(i + x_interval, d_y + MESH_ELEVATION, j);
             
-            temp_points.push(A,B,D,B,C,D);
+            temp_points.push(B_m,A_m,D_m,C_m,B_m,D_m);
             points.push(A,B,D,B,C,D);
-            matrix.push(temp_points);
-            console.log(temp_points);
-            temp_points = [];
-
-            colors.push(a,b,d,b,c,d);
-            x = round(x + x_interval);
+            
         }
-        x = x_min;
-        z = round(z + z_interval);
-        // console.log(points);
+        matrix.push(temp_points);
+        console.log(temp_points);
+        temp_points = [];
     }  
 }
 
-get_patch(-2, 2, -4, 4);
+get_patch(-TERRAIN_BOUNDS, TERRAIN_BOUNDS, -TERRAIN_BOUNDS, TERRAIN_BOUNDS);
 temp = points;
 
 
-sendDataToGPU();
 
 var matWorldUniformLocation = gl.getUniformLocation(program, "mWorld"); // deals with rotation
 var matViewUniformLocation = gl.getUniformLocation(program, "mView"); // deals with camera
@@ -75,38 +102,51 @@ var matProjUniformLocation = gl.getUniformLocation(program, "mProj"); // deals w
 var colorValueUniformLocation = gl.getUniformLocation(program, "colorValue");
 
 var worldMatrix = mat4();
-var viewMatrix = lookAt(vec3(0, 5, 3), vec3(0, 0, 0), vec3(0, 1, 0));
-var projMatrix = perspective(45, canvas.width / canvas.height, 0.1, 1000.0);
-var colorValue = vec3(1.0, 1.0, 1.0);
+var viewMatrix = lookAt(vec3(0, 3, 10), vec3(0, 0, 0), vec3(0, 1, 0));
+var projMatrix = ortho(-10.0, 10.0, 0.0, 10.0, NEAR, FAR);
+var colorValue = vec4(1.0, 1.0, 1.0, 1.0);
 
 // Send matrix data to GPU
 gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, flatten(worldMatrix));
 gl.uniformMatrix4fv(matViewUniformLocation, gl.FALSE, flatten(viewMatrix));
 gl.uniformMatrix4fv(matProjUniformLocation, gl.FALSE, flatten(projMatrix));
-gl.uniform3f(colorValueUniformLocation, colorValue[0], colorValue[1], colorValue[2]);
+gl.uniform4f(colorValueUniformLocation, colorValue[0], colorValue[1], colorValue[2], colorValue[3]);
 
-var angle = 0;
+var motion_x = 0;
+var motion_y = 0;
+var motion_z = 0;
 var identityMatrix = mat4();
 
+// +ve angle means clockwise rotation
+var angle = 0;
+
 var loop = function() {
-    // angle = i / 5 * 2 * Math.PI;
+    // motion_x = i / 60;
+    // motion_y = i / 60;
+    motion_z = i / 60;
 
-    // worldMatrix = rotate(angle, axis_of_rotation); // Rotate by angle
-    // gl.uniformMatrix4fv(matWorldUniformLocation, false, flatten(worldMatrix)); // Send new data to GPU
-    // i++;
+    worldMatrix = rotate(angle, vec3(0, 1, 0));
+    viewMatrix = lookAt(vec3(0 - motion_x, 3 - motion_y, 10 - motion_z), vec3(0 - motion_x, 0 - motion_y, 0 - motion_z), vec3(0, 1, 0));
+    
+    gl.uniformMatrix4fv(matViewUniformLocation, false, flatten(viewMatrix)); // Send new data to GPU
+    gl.uniformMatrix4fv(matWorldUniformLocation, false, flatten(worldMatrix));
 
-    gl.clearColor(0.75, 0.85, 0.8, 1.0);
+    i = i + FLYING_SPEED;
+
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    colorValue = vec3(0.0, 0.0, 0.0);
-    gl.uniform3f(colorValueUniformLocation, colorValue[0], colorValue[1], colorValue[2]);
+    // Drawing Triangles
+    colorValue = vec4(0.0, 0.0, 0.0, 1.0);
+    gl.uniform4f(colorValueUniformLocation, colorValue[0], colorValue[1], colorValue[2], colorValue[3]);
 
     points = temp;
     sendDataToGPU();
     gl.drawArrays(gl.TRIANGLES, 0, points.length);
 
-    colorValue = vec3(1.0, 1.0, 1.0);
-    gl.uniform3f(colorValueUniformLocation, colorValue[0], colorValue[1], colorValue[2]);
+
+    // Drawing Mesh
+    colorValue = vec4(0.75, 0.75, 0.75, 1.0);
+    gl.uniform4f(colorValueUniformLocation, colorValue[0], colorValue[1], colorValue[2], colorValue[3]);
 
     temp = points;
     for (let i = 0; i < matrix.length; i++) {
@@ -117,6 +157,7 @@ var loop = function() {
     
     requestAnimationFrame(loop);
 }
+
 
 requestAnimationFrame(loop);
 
