db8afcdee2b1f87940f4bff7a8261bc472117451
Owais <owaisbinasad.work@gmail.com>
2021-11-09 23:39:20 +0500

    Documentation

--- simulator.js
+++ simulator.js
@@ -5,14 +5,15 @@ let program;
 
 let id = null;
 
-let ENABLE_PERLIN = true;
+let ENABLE_PERLIN = true; // Turn on/off mountains
 let ENABLE_ROUNDING = false;
-let TERRAIN_DETAIL_LEVEL = 300;
-let MESH_ELEVATION = 0;
-let TERRAIN_BOUNDS = 25;
+let TERRAIN_DETAIL_LEVEL = 150; // Increase to improve terrain quality
+let MESH_ELEVATION = 0; // Experimental feature. Better not to try this one out.
+let TERRAIN_BOUNDS = 25; // How big should one terrain patch be
 let PERLIN_SCALING_FACTOR = 2.5;
 let NOISE_SPACER = 3;
 
+// Viewing Volume Bounds
 let RIGHT = 8;
 let LEFT = -7;
 let BOTTOM = -3;
@@ -20,6 +21,7 @@ let TOP = 8;
 let NEAR = 15;
 let FAR = -30.0;
 
+// Flight Control Globals
 let SHIFT_SENSITIVITY = 1;
 let YAW = 0;
 let YAW_SENSITIVITY = 0.1;
@@ -40,11 +42,14 @@ let FLYING_SPEED = 4.0;
 let MAX_SPEED = 10;
 let SPEED_SENSITIVITY = 1;
 
+// Camera globals
 let eye = vec3(0,10,25);
 let at = vec3(0,0,-1);
 let up = vec3 (0,1,0);
 
 let view = "points";
+
+// Choose shading type between "Phong", "Flat", and "Smooth".
 let shade = "Flat";
 
 var points = [];
@@ -60,15 +65,13 @@ gl.viewport(0, 0, canvas.width, canvas.height);
 gl.clearColor(0.75, 0.85, 0.8, 1.0);
 
 gl.enable(gl.DEPTH_TEST);
-// gl.enable(gl.CULL_FACE);
-// gl.frontFace(gl.CCW);
-// gl.cullFace(gl.BACK);
 
 //Place this clear color settings
 gl.polygonOffset(1,1);
 
 //enable or disable this setting as needed
 gl.enable(gl.POLYGON_OFFSET_FILL);
+
 if (shade === "Phong"){
     program = initShaders(gl, "vertex-shader-phong","fragment-shader-phong")
 }
@@ -87,42 +90,6 @@ gl.useProgram(program);
 initGPUPipeline();
 
 gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-function frustum(left, right, bottom, top, near, far) {
-
-    if (left == right) { throw "frustum(): left and right are equal";}
-   
-    if (bottom == top) { throw "frustum(): bottom and top are equal";}
-   
-    if (near == far) { throw "frustum(): near and far are equal";}
-   
-    let w = right - left;
-   
-    let h = top - bottom;
-   
-    let d = far - near;
-   
-    let result = mat4();
-   
-    result[0][0] = 2.0 * near / w;
-   
-    result[1][1] = 2.0 * near / h;
-   
-    result[2][2] = -(far + near) / d;
-   
-    result[0][2] = (right + left) / w;
-   
-    result[1][2] = (top + bottom) / h;
-   
-    result[2][3] = -2 * far * near / d;
-   
-    result[3][2] = -1;
-   
-    result[3][3] = 0.0;
-   
-    return result;
-   
-   }
 
 window.onload = () => {
     document.onkeydown = (e) => {
@@ -231,10 +198,166 @@ window.onload = () => {
     }
 }
 
-
-
 let i = 0;
 
+
+var result = get_patch(-TERRAIN_BOUNDS, TERRAIN_BOUNDS, -TERRAIN_BOUNDS, TERRAIN_BOUNDS);
+points = [...result[0]];
+matrix = [...result[1]];
+normals = [...result[2]];
+matrix_normals = [...result[3]];
+temp = [...points];
+tempnor = [...normals];
+
+
+var matWorldUniformLocation = gl.getUniformLocation(program, "mWorld"); // deals with rotation
+var matViewUniformLocation = gl.getUniformLocation(program, "mView"); // deals with camera
+var matProjUniformLocation = gl.getUniformLocation(program, "mProj"); // deals with 3D to 2D projection
+var matNormalUniformLocation = gl.getUniformLocation(program,"mNorm");
+
+var worldMatrix = mat4();
+var viewMatrix = lookAt(eye,at,up);
+var projMatrix = frustum(LEFT, RIGHT, BOTTOM, TOP, NEAR, FAR);
+var normMatrix = normalMatrix(viewMatrix,false);
+
+// Send matrix data to GPU
+gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, flatten(worldMatrix));
+gl.uniformMatrix4fv(matViewUniformLocation, gl.FALSE, flatten(viewMatrix));
+gl.uniformMatrix4fv(matProjUniformLocation, gl.FALSE, flatten(projMatrix));
+gl.uniformMatrix4fv(matNormalUniformLocation,gl.FALSE, flatten(normMatrix));
+
+
+var identityMatrix = mat4();
+
+
+var z_threshold = 0;
+var terrain_number = 0;
+var z_min = -TERRAIN_BOUNDS;
+var z_max = TERRAIN_BOUNDS;
+
+
+// Animation loop
+var loop = function() {
+    
+    distance = FLYING_SPEED/60;
+    
+    if (rot == true){
+        rotation = mult(rotateZ(ROLL),mult(rotateY(YAW),rotateX(PITCH)));
+        
+        rotation = mat3(rotation[0][0],rotation[0][1],rotation[0][2],
+            rotation[1][0],rotation[1][1],rotation[1][2],
+            rotation[2][0],rotation[2][1],rotation[2][2],);
+    
+        eye = mult(rotation,eye)
+        at = mult(rotation,at)
+        up = mult(rotation,up)  
+        
+        ROLL = 0;
+        YAW = 0;
+        PITCH = 0;
+        rotation = identityMatrix;
+        rot = false;
+        }
+
+    eye = add(eye, mult(distance, normalize(at)));
+    eye[1] = (eye[1] < 3) ? 3 : eye[1];
+    at = add(at, mult(distance, normalize(at)));
+
+    
+    // Checking to see if new terrain should be generated or not
+    if (eye[2] < z_threshold) {
+        
+        terrain_number++;
+
+        z_min = z_min - (2 * TERRAIN_BOUNDS);
+        z_max = z_max - (2 * TERRAIN_BOUNDS);
+
+        console.log(`Generating new terrain with (${-TERRAIN_BOUNDS}, ${TERRAIN_BOUNDS}, ${z_min}, ${z_max}).`);
+        var results = get_patch(-TERRAIN_BOUNDS, TERRAIN_BOUNDS, z_min, z_max);
+        console.log(`Length of new points: ${results[0].length}.`);
+        console.log(`Length of new matrix: ${results[1].length}.`);
+        
+        points = [...points, ...results[0]];
+        temp = [...points];
+        
+        matrix = [...matrix, ...results[1]];
+        
+        normals = [...normals, ...results[2]];
+        tempnor = [...normals];
+
+        matrix_normals = [...matrix_normals, ...results[3]];
+        console.log("Pushing new terrain...");
+
+        z_threshold = z_threshold - (2 * TERRAIN_BOUNDS)
+    }
+    
+    viewMatrix = lookAt(eye, at, up);
+    projMatrix = frustum(LEFT, RIGHT, BOTTOM , TOP , NEAR , FAR);
+    normMatrix = normalMatrix(viewMatrix,false);
+
+    // Send new data to GPU
+    gl.uniformMatrix4fv(matProjUniformLocation, gl.FALSE, flatten(projMatrix));              
+    gl.uniformMatrix4fv(matViewUniformLocation, false, flatten(viewMatrix));
+    gl.uniformMatrix4fv(matWorldUniformLocation, false, flatten(worldMatrix));
+    gl.uniformMatrix4fv(matNormalUniformLocation,false,flatten(normMatrix));
+    i+= 1;
+
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+   
+    points = temp;
+    normals = tempnor;
+    sendDataToGPU();
+
+    if (view === "faces"){
+        gl.drawArrays(gl.TRIANGLES, 0, points.length);
+    }
+    
+
+    else if (view === "wireframe"){
+        for (let i = 0; i < matrix.length; i++) {
+            normals = matrix_normals[i];
+            points = matrix[i];
+            sendDataToGPU();
+            gl.drawArrays(gl.LINE_STRIP, 0, points.length);
+        }
+    }
+
+    else if (view === "points"){
+        for (let i = 0; i < matrix.length; i++) {
+            normals = matrix_normals[i];
+            points = matrix[i];
+            sendDataToGPU();
+            gl.drawArrays(gl.POINTS, 0, points.length);
+        }
+    }
+    
+    id = requestAnimationFrame(loop);
+}
+
+
+id = requestAnimationFrame(loop);
+
+
+function initGPUPipeline() {
+    vPosition = gl.getAttribLocation(program, "vPosition");
+    vNormal = gl.getAttribLocation(program, "vNormal");
+}
+  
+
+function sendDataToGPU() {
+    // Send vertex data to GPU
+    gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STREAM_DRAW);
+
+    gl.vertexAttribPointer(vPosition, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(normals), gl.STATIC_DRAW);
+
+    gl.vertexAttribPointer(vNormal, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vNormal);
+}
 
 function get_patch(x_min, x_max, z_min, z_max){
     
@@ -292,174 +415,38 @@ function get_patch(x_min, x_max, z_min, z_max){
     return [_points, _matrix, _normals, _matrix_normals]
 }
 
-var result = get_patch(-TERRAIN_BOUNDS, TERRAIN_BOUNDS, -TERRAIN_BOUNDS, TERRAIN_BOUNDS);
-points = [...result[0]];
-matrix = [...result[1]];
-normals = [...result[2]];
-matrix_normals = [...result[3]];
-temp = [...points];
-tempnor = [...normals];
+function frustum(left, right, bottom, top, near, far) {
 
-
-var matWorldUniformLocation = gl.getUniformLocation(program, "mWorld"); // deals with rotation
-var matViewUniformLocation = gl.getUniformLocation(program, "mView"); // deals with camera
-var matProjUniformLocation = gl.getUniformLocation(program, "mProj"); // deals with 3D to 2D projection
-var matNormalUniformLocation = gl.getUniformLocation(program,"mNorm");
-
-var worldMatrix = mat4();
-var viewMatrix = lookAt(eye,at,up);
-var projMatrix = frustum(LEFT, RIGHT, BOTTOM, TOP, NEAR, FAR);
-var normMatrix = normalMatrix(viewMatrix,false);
-
-// Send matrix data to GPU
-gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, flatten(worldMatrix));
-gl.uniformMatrix4fv(matViewUniformLocation, gl.FALSE, flatten(viewMatrix));
-gl.uniformMatrix4fv(matProjUniformLocation, gl.FALSE, flatten(projMatrix));
-gl.uniformMatrix4fv(matNormalUniformLocation,gl.FALSE, flatten(normMatrix));
-
-
-var identityMatrix = mat4();
-
-
-var z_threshold = 0;
-var terrain_number = 0;
-var z_min = -TERRAIN_BOUNDS;
-var z_max = TERRAIN_BOUNDS;
-
-
-var loop = function() {
-    
-    distance = FLYING_SPEED/60;
-    
-    if (rot == true){
-        rotation = mult(rotateZ(ROLL),mult(rotateY(YAW),rotateX(PITCH)));
-        
-        rotation = mat3(rotation[0][0],rotation[0][1],rotation[0][2],
-            rotation[1][0],rotation[1][1],rotation[1][2],
-            rotation[2][0],rotation[2][1],rotation[2][2],);
-    
-        eye = mult(rotation,eye)
-        at = mult(rotation,at)
-        up = mult(rotation,up)  
-        
-        ROLL = 0;
-        YAW = 0;
-        PITCH = 0;
-        rotation = identityMatrix;
-        rot = false;
-        }
-
-    eye = add(eye, mult(distance, normalize(at)));
-    at = add(at, mult(distance, normalize(at)));
-
-    
-    // console.log(updatedEye[2]);
-
-    if (eye[2] < z_threshold) {
-        
-        terrain_number++;
-
-        z_min = z_min - (2 * TERRAIN_BOUNDS);
-        z_max = z_max - (2 * TERRAIN_BOUNDS);
-
-        console.log(`Generating new terrain with (${-TERRAIN_BOUNDS}, ${TERRAIN_BOUNDS}, ${z_min}, ${z_max}).`);
-        var results = get_patch(-TERRAIN_BOUNDS, TERRAIN_BOUNDS, z_min, z_max);
-        console.log(`Length of new points: ${results[0].length}.`);
-        console.log(`Length of new matrix: ${results[1].length}.`);
-        
-        points = [...points, ...results[0]];
-        temp = [...points];
-        
-        matrix = [...matrix, ...results[1]];
-        
-        normals = [...normals, ...results[2]];
-        tempnor = [...normals];
-
-        matrix_normals = [...matrix_normals, ...results[3]];
-        console.log("Pushing new terrain...");
-
-        z_threshold = z_threshold - (2 * TERRAIN_BOUNDS)
-    }
-    
-    viewMatrix = lookAt(eye, at, up);
-    projMatrix = frustum(LEFT, RIGHT, BOTTOM , TOP , NEAR , FAR);
-    normMatrix = normalMatrix(viewMatrix,false);
-
-    gl.uniformMatrix4fv(matProjUniformLocation, gl.FALSE, flatten(projMatrix));              
-    gl.uniformMatrix4fv(matViewUniformLocation, false, flatten(viewMatrix)); // Send new data to GPU
-    gl.uniformMatrix4fv(matWorldUniformLocation, false, flatten(worldMatrix));
-    gl.uniformMatrix4fv(matNormalUniformLocation,false,flatten(normMatrix));
-    i+= 1;
-
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-    // Drawing Triangles
+    if (left == right) { throw "frustum(): left and right are equal";}
    
-    points = temp;
-    normals = tempnor;
-    sendDataToGPU();
-
-    if (view === "faces"){
-        gl.drawArrays(gl.TRIANGLES, 0, points.length);
-    }
-    
-
-    else if (view === "wireframe"){
-        for (let i = 0; i < matrix.length; i++) {
-            normals = matrix_normals[i];
-            points = matrix[i];
-            sendDataToGPU();
-            gl.drawArrays(gl.LINE_STRIP, 0, points.length);
-        }
-    }
-
-    else if (view === "points"){
-        for (let i = 0; i < matrix.length; i++) {
-            normals = matrix_normals[i];
-            points = matrix[i];
-            sendDataToGPU();
-            gl.drawArrays(gl.POINTS, 0, points.length);
-        }
-    }
-    
-    id = requestAnimationFrame(loop);
-}
-
-
-id = requestAnimationFrame(loop);
-
-
-function initGPUPipeline() {
-    vPosition = gl.getAttribLocation(program, "vPosition");
-    vNormal = gl.getAttribLocation(program, "vNormal");
-}
-
-    
-
-function sendDataToGPU() {
-    // Send vertex data to GPU
-    gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STREAM_DRAW);
-
-    gl.vertexAttribPointer(vPosition, 3, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vPosition);
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(normals), gl.STATIC_DRAW);
-
-    gl.vertexAttribPointer(vNormal, 3, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vNormal);
-}
-
-function round(n) {
-    if (ENABLE_ROUNDING) {
-    var temp = n.toFixed(1);
-    if (temp == "0.0") {
-        return 0.0;
-    } else {
-        return parseFloat(temp);
-    }
-    } else {
-        return n;
-    }
+    if (bottom == top) { throw "frustum(): bottom and top are equal";}
+   
+    if (near == far) { throw "frustum(): near and far are equal";}
+   
+    let w = right - left;
+   
+    let h = top - bottom;
+   
+    let d = far - near;
+   
+    let result = mat4();
+   
+    result[0][0] = 2.0 * near / w;
+   
+    result[1][1] = 2.0 * near / h;
+   
+    result[2][2] = -(far + near) / d;
+   
+    result[0][2] = (right + left) / w;
+   
+    result[1][2] = (top + bottom) / h;
+   
+    result[2][3] = -2 * far * near / d;
+   
+    result[3][2] = -1;
+   
+    result[3][3] = 0.0;
+   
+    return result;
+   
 }
